adb475f78d299cdc24c8def4410a447a14965f20
Sam Warnick <sam.t.warnick@gmail.com>
2018-06-06 06:05:44 -0600

    Initial Commit

---
+++ Decider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Decider
+//
+//  Created by Sam Warnick on 6/6/18.
+//  Copyright © 2018 Sam Warnick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
