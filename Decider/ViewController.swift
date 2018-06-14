efcf9d59bce61e6fac2100313a7bad5aad698ccd
Sam Warnick <sam.t.warnick@gmail.com>
2018-06-14 06:44:05 -0600

    Initial map search capabilities

--- Decider/ViewController.swift
+++ Decider/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     
     // MARK: - Properties
     private let locationManager = CLLocationManager()
+//    private var search = MKLocalSearch()
+    private let completer = MKLocalSearchCompleter()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -25,11 +27,8 @@ class ViewController: UIViewController {
         setupSeachBar()
         setupMapView()
         
-        if CLLocationManager.locationServicesEnabled() {
-            findUserLocation();
-        } else {
-            locationManager.requestWhenInUseAuthorization()
-        }
+        completer.delegate = self
+        completer.region = mapView.region
     }
     
     // MARK: - Private Functions
@@ -41,7 +40,16 @@ class ViewController: UIViewController {
     }
     
     private func setupMapView() {
+        if CLLocationManager.locationServicesEnabled() {
+            findUserLocation();
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
         mapView.showsUserLocation = true
+//        mapView.showsCompass = true
+//        mapView.showsScale = true
+        mapView.showsPointsOfInterest = true
     }
     
     private func findUserLocation() {
@@ -49,11 +57,59 @@ class ViewController: UIViewController {
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.requestLocation()
     }
+    
+    // MARK: - Actions
+    @IBAction func goToTopResult() {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = completer.results.first?.title
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        mapView.removeAnnotations(mapView.annotations)
+        search.start() { response, _ in
+            guard let response = response else { return }
+            let annotations: [MKPointAnnotation] = response.mapItems.map { mapItem in
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                return annotation
+            }
+            self.mapView.addAnnotations(annotations)
+            if let coordinates = annotations.first?.coordinate {
+                let region = MKCoordinateRegion(center: coordinates, latitudinalMeters: 1000, longitudinalMeters: 1000)
+                self.mapView.setRegion(region, animated: true)
+            }
+        }
+    }
+    
 }
 
 // MARK: - UISearchBarDelegate
 extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        completer.queryFragment = searchText
+    }
     
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = completer.results.first?.title
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        mapView.removeAnnotations(mapView.annotations)
+        var annotations = [MKPointAnnotation]()
+        search.start() { response, _ in
+            guard let response = response else { return }
+            annotations.append(contentsOf: response.mapItems.map { mapItem in
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                return annotation
+            })
+        }
+        
+        mapView.addAnnotations(annotations)
+        if let coordinates = annotations.first?.coordinate {
+            let region = MKCoordinateRegion(center: coordinates, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            mapView.setRegion(region, animated: true)
+        }
+    }
 }
 
 // MARK: - CLLocationManagerDelegate
@@ -66,11 +122,17 @@ extension ViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let coordinates = manager.location?.coordinate else { return }
-        let region = MKCoordinateRegion(center: coordinates, latitudinalMeters: 500, longitudinalMeters: 500)
+        let region = MKCoordinateRegion(center: coordinates, latitudinalMeters: 1000, longitudinalMeters: 1000)
         mapView.setRegion(region, animated: true)
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("\(error)")
+    }
+}
+
+extension ViewController: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+//        topResultButton.titleLabel?.text = completer.results.first?.title
     }
 }
